4df669ab0ca08e3b43e0c3e5182921f6772ccaf5
GertrudaSK <penkta@gmail.com>
2020-08-27 01:19:35 +0300

    max length from 17 and greater works fine

--- msg/msg.py
+++ msg/msg.py
@@ -1,63 +1,127 @@
 import math
 import re
 
-def clean_string(text_list: list) -> list:
-    final_text = []
-    length = len(text_list)
-    n = 1
-    for item in text_list:
-        final_text.append(f"{item}({n}/{length})")
-        n = n + 1
-    return final_text
 
-def fitted_text(text: str, max_length=30) -> list:
-    text_chars = len(text)
-    add_space = math.ceil((text_chars/max_length*5+text_chars)/max_length)
-    tail = 5 + 2 * (len(str(add_space))-1)
-    optimal_length = max_length - tail - 5
-    start = 0
-    end = 0
-    clean_strings = []
-    bad_words = []
-    for i in range(0, len(text), optimal_length):
-        fixing_text = []
-        end += optimal_length
-        string = text[start:end]
-        fixing_text.append(string)
-        start += optimal_length
-        clean_text = re.compile(r'.+\s')
+class Message_Splitter:
 
-        try:
-            if bad_words:
-                full_word = bad_words[0] + fixing_text[0]
-                fixing_text[0] = full_word
-                bad_words = []
-            res = clean_text.search(fixing_text[0])
-            result = res.group()
+    def __init__(self, text, max_length):
+        self.text = text
+        self.max_length = max_length
 
-            if result[-1] == " ":
-                result = result[:-1]
-                clean = f"{result} "
+    def format_msg(self):
+        text_chars = len(self.text)
+        msg_count = math.ceil(text_chars / self.max_length)
+        msg_count_with_tail = math.ceil(
+            (msg_count * 6 + text_chars) / self.max_length)
+        tail = 6 + 2 * (len(str(msg_count_with_tail)) - 1)
+        optimal_length = self.max_length - tail
 
-            clean_strings.append(clean)
-            muted_text = fixing_text[0].replace(clean, "")
-            fixing_text[0] = muted_text
-            bad_words.append(fixing_text[0])
+        good_text = []
+        text_cleaned = self.text.split(" ")
 
-        except AttributeError:
-            clean_strings.append(fixing_text[0])
+        msg_fitted = ""
 
-    if bad_words:
-        clean_strings.append(f'{bad_words[0]} ')
-    if len(clean_strings[-1]) + len(clean_strings[-2]) <= optimal_length:
-        clean_strings[-2] = clean_strings[-2] + clean_strings[-1]
-        clean_strings.pop()
-    text_fixed = clean_string(clean_strings)
-    return text_fixed
+        def add_text(max_len=self.max_length):
+            striped_text = msg_fitted.strip()
+            if striped_text != "":
+                good_text.append(striped_text)
+            else:
+                pass
+
+        def add_message(message):
+            msg_fitted + message.strip()
+
+        for word in text_cleaned:
+            if len(f'{msg_fitted} {word}') <= optimal_length:
+                msg_fitted = f'{msg_fitted} {word}'
+                add_message(msg_fitted)
+            elif len(f'{msg_fitted} {word}') > optimal_length:
+                add_text()
+                msg_fitted = word
+
+        if msg_fitted:
+            add_message(msg_fitted)
+            add_text()
+
+        def add_tail(text_list: list, max_length, length=None) -> list:
+            def chck_max_msg(final_text):
+                length = len(final_text)
+                pattern = re.compile(r'(\d+)(?!.*\d)')
+                res = pattern.search(final_text[0])
+                result = res.group()
+                if len(final_text) != int(result):
+                    new_list = []
+                    for item in final_text:
+                        x = re.sub('(\d+)(?!.*\d)', str(length), item)
+                        new_list.append(x)
+                    return new_list
+                return final_text
+
+            final_text = []
+            if length == None:
+                length = len(text_list)
+            n = 1
+            for item in text_list:
+
+                index_item = text_list.index(item)
+
+                final_text.append(f"{item} ({n}/{length})")
+                n = n + 1
+
+                if len(final_text[-1]) <= max_length:
+                    if index_item + 1 <= text_list.index(text_list[-1]):
+                        # try to add next element and check if does it fit
+
+                        if len(
+                                f'{final_text[-1]} {text_list[index_item + 1]}') <= max_length:
+                            split_msg = final_text[-1].split()
+                            split_msg.insert(-1, text_list[index_item + 1])
+                            final_text[-1] = " ".join(
+                                split_msg)  # insert additional word into final_text list
+                            text_list[index_item + 1] = text_list[
+                                index_item + 1].replace(
+                                text_list[index_item + 1],
+                                "").strip()  # delete inserted word from text_list
+                            if text_list[
+                                index_item + 1] == "":  # if text_list item is empty after deleting - item popped
+                                text_list.pop(index_item + 1)
+
+                            # if next word from next message fits - fixing text_list
+                            string_parts = final_text[-1].split()
+                            result = string_parts[-2]  # before tail
+
+                            if text_list[index_item] == text_list[-1]:
+                                text_list.append("")
+
+                            text_list[
+                                index_item + 1] = f'{result} {text_list[index_item + 1]}'.strip()  # insert into text_list additional word before tail
+                            text_list[index_item] = text_list[
+                                index_item].replace(result, "").strip()
+                            final_text[-1] = final_text[-1].replace(result,
+                                                                    "").replace(
+                                "  ", " ")
+
+                elif len(final_text[-1]) > max_length:
+                    # doesn't fit one long word
+                    words_list = final_text[-1].split()
+                    tail_len = len(words_list[-1])
+                    cut_word_edge = max_length - tail_len - 1
+                    splitted_word = words_list[0][:cut_word_edge]
+                    final_text[-1] = " ".join([splitted_word, words_list[-1]])
+
+                    try:
+                        text_list[
+                            index_item + 1] = f'{words_list[0][cut_word_edge:]} {text_list[index_item + 1]}'.strip()  # insert into text_list additional word before tail
+                    except IndexError:
+                        text_list.append(
+                            f'{words_list[0][cut_word_edge:]}')  # insert into text_list additional word before tail
+
+            return chck_max_msg(final_text)
+
+        return add_tail(good_text, max_length=self.max_length)
 
 
 if __name__ == '__main__':
-
-    print(fitted_text('Splits long message to multiple messages in order to fit within an arbitrary message length limit (useful for SMS, Twitter, etc.).', 22))
-
-
+    print(Message_Splitter(
+        "Split multiple messages in order to fit within an arbitrary message length limit (useful for SMS, Twitter, etc.)..",
+        35).format_msg())
