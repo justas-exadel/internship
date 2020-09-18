fda48e3d4f6254c60f7fe825111cc52f120d9aea
GertrudaSK <penkta@gmail.com>
2020-09-18 17:12:59 +0300

    Update Message Splitter

--- msg_flask/forms.py
+++ msg_flask/forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired, NumberRange
 class Message_Form(FlaskForm):
     text = TextAreaField('Message Text', [DataRequired()],
                          render_kw={'class': 'form-control', 'rows': 10})
-    max_length = IntegerField('Length', validators=[NumberRange(min=12, message='Message length have to be greater than 10')])
+    max_length = IntegerField('Length', validators=[NumberRange(min=11, message='Message length have to be greater than 10')])
     submit = SubmitField('Submit', render_kw={'class': 'btn btn-success'})
